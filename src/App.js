63a5ee6e54fd5b0f38f53e3e7f1803eeffbd6c84
Ana Ceprnja <aceprnja@iun.edu>
2021-02-23 13:11:38 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+//name and store variables before reutnr statement and use curly brackets 
 function App() {
+  const post = {
+    title: "Dinosaurs are awesome",
+    author: "Stealthy Stegosaurus",
+    body: "Check out this body property!",
+    comments: ["First!", "Great post", "Hire this author now!"]
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{post.title}</h2>
+      <p>by:{post.author}</p>
+      <p>{post.body}</p>
+      <h3>Comments:</h3>
+      <p>{post.comments[0]}</p>
     </div>
   );
 }
